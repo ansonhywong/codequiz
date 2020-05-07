d9a205b852442fe8b779cd9244c3fd5c31338858
Anson Wong <ansonhywong@gmail.com>
2020-05-07 01:03:24 -0400

    first commit

---
+++ questions.js
@@ -0,0 +1,77 @@
+
+
+//Questions sourced from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JavaScript
+
+var questions = [
+    {
+        title: "1. What is HTML?",
+        choices: ["HTML stands for Hyper Text Markup Language.",
+            "HTML stands for High Text Motion Language.",
+            "HTML stands for Height Title Media Length.",
+            "HTML stands for Hey Text Me Later."],
+        answer: "HTML stands for Hyper Text Markup Language."
+    },
+    {
+        title: "2. Do all HTML tags have an end tag?",
+        choices: ["Yes, without exceptions.",
+        "No, there are some HTML tags that don't need a closing tag. ( <image> , <br> ).",
+        "No, there are some HTML tags that don't need a closing tag. ( <header> , <footer> ).",
+        "No, only <html>, <head>, and <body> tags need a closing tag."],
+        answer: "No, there are some HTML tags that don't need a closing tag. ( <image> , <br> )."
+    },
+    {
+        title: "3. What is JavaScript?",
+        choices: ["JavaScript is a complete programming language.",
+        "JavaScript is a Java library.",
+        "JavaScript is an object-oriented programming (OOPS).",
+        "JavaScript is a client-side scripting language."],
+        answer: "JavaScript is a client-side scripting language."
+    },
+    {
+        title: "4. How do you write 'Hello World' in an alert box?",
+        choices: ["msgBox('Hello World)", "alertBox('Hello World')", "alert('Hello World')", "msg('Hello World)"],
+        answer: "alert('Hello World')"
+    },
+    {
+        title: "5. How do you call a function called myFunction?",
+        choices: ["myFunction()", "call myFunction()", "call function myFunction", "call select myFunction"],
+        answer: "myFunction()"
+    },
+    {
+        title: "6. What are JavaScript Data Types?",
+        choices: ["Integer  |  Float   |  Double   |  Boolean",
+        "Database |  Bigdata",
+        "Array    |  List    |  String   |  Object",
+        "Number   |  String  |  Boolean  |  Object  |  Undefined"],
+        answer: "Number   |  String  |  Boolean  |  Object  |  Undefined"
+    },
+    {
+        title: "7. How does a while loop start?",
+        choices: ["while i between 0 and 10", "while (i <= 10; i++)", "while (i <= 10)", "while i <= 10"],
+        answer: "while (i <= 10)"
+    },
+    {
+        title: "8. Which company developed JavaScript?",
+        choices: ["WebCoders is the software company who developed JavaScript.",
+        "Netscape is the software company who developed JavaScript.",
+        "JavaScript was not developed by a company.",
+        "Ubisoft is the software company who developed JavaScript."],
+        answer: "Netscape is the software company who developed JavaScript."
+    },
+    {
+        title: "9. What is '===' operator?",
+        choices: ["'===' is an attribution operator used in Javascript.",
+        "'===' is a strict equality operator which returns true when the two operands are having the same value without any type conversion.",
+        "'===' is not a valid equality operator, '==' is the correct equality operator.",
+        "'===' is not an operator, but a comment tag."],
+        answer: "'===' is a strict equality operator which returns true when the two operands are having the same value without any type conversion."
+    },
+    {
+        title: "10. What is jQuery?",
+        choices: ["jQuery is a programming language.",
+        "jQuery is a built-in library built for JavaScript.",
+        "jQuery is the replacement of JavaScript.",
+        "jQuery is a curse to web developers."],
+        answer: "jQuery is a built-in library built for JavaScript."
+    }
+]
